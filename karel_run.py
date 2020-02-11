c5cd325cdb2ec7ce9a74f25e81cc9fa4db34d9ee
xsebek <xsebek@fi.muni.cz>
2020-02-11 04:50:12 +0100

    Add simple documentation to exported functions

--- karel_run.py
+++ karel_run.py
@@ -14,15 +14,15 @@ try:
         karel_help()
         exit(1)
 except IOError as e:
-    print("Could not open karel map file.", file=sys.stderr)
+    print("Could not open karel map file:\n" + str(e))
     exit(2)
 except RuntimeError as r:
-    print("Failed parsing map:\n", file=sys.stderr)
-    print(r, file=sys.stderr)
+    print("Failed parsing map:\n" + str(r))
     exit(3)
 
 
 def refresh(func, moved=False):
+    """Execute a Board method and redraw. """
     try:
         res = func()
         _b.redraw(moved)
@@ -36,27 +36,73 @@ def refresh(func, moved=False):
 ###########################################################
 
 # Movement
-def move(): refresh(_b.move, True)
-def turn_left(): refresh(_b.karel.turn_left)
-def turn_right(): refresh(_b.karel.turn_right)
-def stop(): _b.complete()
-def pause(): _b.pause()
+def move():
+    """Karel moves in the direction he is facing. """
+    refresh(_b.move, True)
+
+def turn_left():
+    """Karel turns left. """
+    refresh(_b.karel.turn_left)
+
+def turn_right():
+    """Karel turns right. """
+    refresh(_b.karel.turn_right)
+
 # Beepers
-def pick_beeper(): refresh(_b.pick_beeper)
-def put_beeper(): refresh(_b.put_beeper)
-def beeper_is_present(): return _b.beeper_is_present()
+def pick_beeper():
+    """Karel tries to pick up a beeper. """
+    refresh(_b.pick_beeper)
+
+def put_beeper():
+    """Karel puts down a beeper (if he has any). """
+    refresh(_b.put_beeper)
+
+def beeper_is_present():
+    """True iff Karel stands on a beeper. """
+    return _b.beeper_is_present()
+
 # Walls
-def front_is_blocked(): return _b.front_is_blocked()
-def front_is_treasure(): return _b.front_is_treasure()
+def front_is_blocked():
+    """True iff Karel can't move forward. """
+    return _b.front_is_blocked()
+
+def front_is_treasure():
+    """True if Karel is standing in front of a Treasure. """
+    return _b.front_is_treasure()
+
 # Direction
-def facing_north(): return _b.karel.facing_north()
-def facing_south(): return _b.karel.facing_south()
-def facing_east(): return _b.karel.facing_east()
-def facing_west(): return _b.karel.facing_west()
-# Settings
-def set_speed(spd): _b.speed = _b.valid_speed(spd)
-def set_karel_beepers(b=0): _b.karel.beepers = b if b is None \
-                                                 else max(0, round(b))
+def facing_north():
+    """True iff Karel is facing north (^). """
+    return _b.karel.facing_north()
+
+def facing_south():
+    """True iff Karel is facing south (v). """
+    return _b.karel.facing_south()
+
+def facing_east():
+    """True iff Karel is facing east (>). """
+    return _b.karel.facing_east()
+
+def facing_west():
+    """True iff Karel is facing west (<). """
+    return _b.karel.facing_west()
+
+# Execution
+def set_speed(spd):
+    """Set how fast Karel moves (0 to 100). """
+    _b.speed = _b.valid_speed(spd)
+
+def set_karel_beepers(b=0):
+    """Set Karel's beepers, 0+ and None means inf. """
+    _b.karel.beepers = b if b is None else max(0, round(b))
+
+def stop():
+    """End execution. """
+    _b.complete()
+
+def pause():
+    """Pause execution, press any key to continue. """
+    _b.pause()
 
 
 ###########################################################
@@ -64,6 +110,7 @@ def set_karel_beepers(b=0): _b.karel.beepers = b if b is None \
 ###########################################################
 
 def interactive():
+    """Command the created Board using your keyboard. """
     set_speed(100)  # do not wait
     curses.noecho()
     curses.cbreak()
